c389232f98632f76bf31dae13740ce0d3ed9e399
liup215 <2256935429@qq.com>
2017-08-12 00:07:32 +0800

    引入datatables插件

--- src/main/webapp/static/modular/base/grade/grade.js
+++ src/main/webapp/static/modular/base/grade/grade.js
@@ -1,30 +1,37 @@
-layui.use(['form','jquery','table'],function () {
+layui.use(['form','jquery','dataTables'],function () {
     window.jQuery = window.$ = layui.jquery;
     var Grade = {
         id:"gradeTable"
     }
 
-    Grade.initColumn = function(){
-        var columns = [
-            {title: 'id', field: 'id'},
-            {title: '年级名称', field: 'name'},
-            {title: '学段', field: 'level'},
-            {title: '状态', field: 'status'}
-        ]
-        return columns;
-    }
-
     $(function () {
-        var defaultColumn = Grade.initColumn();
-
-        var table = layui.table({
-            elem:Grade.id,
-            url:'/grade/list',
-            type:'GET',
-            defaultColumn:defaultColumn,
-        })
-
-        table.init();
+        // var dataTable = layui.dataTable;
+        $('#example').dataTable( {
+            language: {
+            "sProcessing": "处理中...",
+            "sLengthMenu": "显示 _MENU_ 项结果",
+            "sZeroRecords": "没有匹配结果",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sSearch": "搜索:",
+            "sUrl": "",
+            "sEmptyTable": "表中数据为空",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        }
+        } );
 
     })
 })
