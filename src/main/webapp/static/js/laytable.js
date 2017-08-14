e6517320d13a09b9bc842e7ec9ff0c6a0e8f17d9
liupeng <2256935429@qq.com>
2017-08-14 19:28:25 +0800

    使用layui的laypage模块建立table模块，有少量bug

--- src/main/webapp/static/js/laytable.js
+++ src/main/webapp/static/js/laytable.js
@@ -1,14 +1,21 @@
 /**
  * Created by Administrator on 2017/8/12.
  */
-layui.define(['jquery','dataTables'],function (exports) {
+layui.define(['jquery','laypage'],function (exports) {
     window.$ = layui.jquery;
+    var laypage = layui.laypage;
 
     var LayTable = function (options) {
         this.config = {
-            elem:undefined
+            table:undefined,
+            page:undefined,
+            nums:undefined,
+            columns:undefined
         }
     };
+
+    var seItem = null;
+    var currentPage = -1;
 
     var TABLE = {};
 
@@ -21,41 +28,90 @@ layui.define(['jquery','dataTables'],function (exports) {
     LayTable.prototype.init = function () {
         var _this = this;
         var _config = _this.config;
-        if(typeof(_config.elem) !== 'string' && typeof(_config.elem) !== 'object') {
+        if(typeof(_config.table) !== 'string' && typeof(_config.table) !== 'object') {
             layer.alert('Table错误提示: elem参数未定义或设置出错');
         }
-        var $container;
-        if(typeof(_config.elem) === 'string') {
-            $container = $('#' + _config.elem + '');
+        var $tableContainer;
+        if(typeof(_config.table) === 'string') {
+            $tableContainer = $('#' + _config.table + '');
         }
-        if(typeof(_config.elem) === 'object') {
-            $container = _config.elem;
+        if(typeof(_config.table) === 'object') {
+            $tableContainer = _config.table;
         }
-        if($container.length === 0) {
+        if($tableContainer.length === 0) {
             layer.alert('Table错误提示:找不到elem参数配置的容器，请检查.');
         }
-        TABLE.container = $container;
-        TABLE.columns = _config.columns;
-        TABLE.data = ajaxData(_config.ajax);
 
+        TABLE.tableContainer = $tableContainer;
+        TABLE.columns = _config.columns;
+        TABLE.data = TABLE.ajaxData(_config.ajax);
+        TABLE.nums = _config.nums;
+        TABLE.page = _config.page;
+        TABLE.table = _config.table;
+
+        TABLE.tableContainer.append("<thead><tr></tr></thead>");
+
+        layui.each(TABLE.columns,function (index,item) {
+            $("#"+_config.table +" thead tr").append("<td>"+item.name+"</td>");
+        })
+        TABLE.tableContainer.append("<tbody id = '"+_config.table+"Body'></tbody>");
+
+        return _this;
     };
 
     LayTable.prototype.getTable = function(){
-        if(TABLE.container === undefined){
+        if(TABLE.tableContainer===undefined){
             this.init();
-            alert("table初始化成功")
         }
-        TABLE.container.DataTable({
-            info:false,
-            paging:false,
-            searching:false,
-            data:TABLE.data,
-            columns:TABLE.columns
+        laypage({
+            cont:TABLE.page,
+            pages:Math.ceil(TABLE.data.length/TABLE.nums),
+            skip:true,
+            jump:function (obj) {
+                currentPage = obj.curr;
+                document.getElementById(TABLE.table + 'Body').innerHTML = TABLE.render(TABLE.columns,TABLE.data,obj.curr);
+            }
         });
+
+        //监听选中
+        $("tbody tr").on('click',function () {
+            if($(this).hasClass('selected')){
+                $(this).removeClass('selected').css('background','');
+                seItem = null;
+            }else{
+                $(this).siblings('tr').each(function () {
+                    if($(this).hasClass('selected')){
+                        $(this).removeClass('selected').css('background','');
+                    }
+                })
+                $(this).addClass('selected').css('background','#F1F2F7');
+
+                alert($(this).index());
+                // for(var index in TABLE.data){
+                //     if(TABLE.data[index].id==seId){
+                //         seItem = TABLE.data[index];
+                //     }
+                // }
+            }
+        })
+
     };
 
 
-    function ajaxData(options){
+
+    LayTable.prototype.getSelectItem = function () {
+        return seItem;
+    };
+
+    LayTable.prototype.refresh = function(){
+        alert("aaa")
+        $("#"+TABLE.table).empty();
+        $("#"+TABLE.page).empty();
+        this.init();
+        this.getTable();
+    };
+
+    TABLE.ajaxData = function(options){
         var content = null;
         $.ajax({
             url:options.url,
@@ -66,6 +122,26 @@ layui.define(['jquery','dataTables'],function (exports) {
             async:false
         })
         return content;
+    }
+
+    TABLE.render = function(columns,data,curr) {
+        var arr = [],
+            items = [],
+            thisData = data.concat().splice(curr*TABLE.nums-TABLE.nums,TABLE.nums);
+        layui.each(thisData,function (index,item) {
+            items=[];
+            layui.each(columns,function (inx,itm) {
+                var key = itm.field;
+                for(var i in item){
+                    if(i==key){
+                        items.push("<td>"+item[i]+"</td>")
+                    }
+                }
+
+            });
+            arr.push("<tr>"+items.join('')+"</tr>");
+        });
+        return arr.join('')
     }
     var laytable = new LayTable();
     exports('laytable',function (options) {
--- src/main/webapp/static/modular/base/grade/grade.js
+++ src/main/webapp/static/modular/base/grade/grade.js
@@ -1,30 +1,96 @@
 layui.use(['form','jquery','laytable'],function () {
     window.jQuery = window.$ = layui.jquery;
+    var layer = layui.layer,
+        form = layui.form();
 
     var Grade = {
-        elem:"gradeTable"
+        table:"gradeTable",
+        page:"gradePage",
+        nums:2,
+        seItemId:null,
+        layerIndex : -1
     };
+
+    //初始化表格列
     Grade.initColumn =function () {
         var columns = [
-        {data: 'id'},
-        {data: 'name'},
-        {data: 'level'},
-        {data: 'status'}]
-    return columns;
+        {name:'id',field: 'id'},
+        {name:'年级名称',field: 'name'},
+        {name:'学段',field: 'level'},
+        {name:'状态',field: 'status'}]
+        return columns;
     };
+
+
 
     var defaultColumn = Grade.initColumn();
 
-    var table = layui.laytable({
-        elem:Grade.elem,
+    var laytable = layui.laytable({
+        table:Grade.table,
+        page:Grade.page,
+        nums:Grade.nums,
         columns:defaultColumn,
         ajax:{
             url:'/grade/list',
             type:'GET'
         }
     });
+    laytable.getTable();
 
-    table.getTable();
+    //检查是否选中
+    Grade.check = function () {
+        var selected = laytable.getSelectItem();
+        if (selected== null) {
+            layer.msg("请选中一项");
+            return false;
+        } else {
+            Grade.seItem = selected;
+            return true;
+        }
+    };
+
+
+    form.on('submit(gradeAdd)',function () {
+        var index = layer.open({
+            type: 2,
+            title: '添加菜单',
+            area: ['830px', '450px'], //宽高
+            fix: false, //不固定
+            maxmin: true,
+            content:'/grade/grade_add'
+        });
+        Grade.layerIndex = index;
+    })
+
+    form.on('submit(gradeChange)',function () {
+        if (Grade.check()) {
+            var index = layer.open({
+                type: 2,
+                title: '修改菜单',
+                area: ['800px', '450px'], //宽高
+                fix: false, //不固定
+                maxmin: true,
+                content: '/grade/grade_edit/' + Grade.seItem.id
+            });
+            Grade.layerIndex = index;
+        }
+    })
+
+    form.on('submit(gradeDelete)',function () {
+        if (Grade.check()) {
+            layer.confirm("是否刪除年级?", function (index) {
+                $.ajax({
+                    url:'/grade/remove',
+                    data:{gradeId:Grade.seItem.id},
+                    success:function (data) {
+                        layer.msg("删除成功！");
+                        laytable.refresh();
+                    }
+                })
+                layer.close(index);
+            });
+        }
+    })
 
 })
 
